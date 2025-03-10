81b418fd5d54aff31c1ffb2c76792221b6156df0
Ismail-Rahib <111448964+Ismail-Rahib@users.noreply.github.com>
2025-03-10 18:08:58 +0530

    Twilio.php

--- app/Config/Twilio.php
+++ app/Config/Twilio.php
@@ -3,7 +3,7 @@ namespace Config;
 
 class Twilio
 {
-    public $sid = 'AC1e3d1fd191a8d2ac40eba7940c000597';
-    public $token = '90a939bf6e7b480133d544e23a7f43fe';
-    public $from = '+12292672168';
+    public $sid = '';
+    public $token = '';
+    public $from = '';
 }
